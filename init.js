bec654202621fba596535561846a190130ec617e
lijiawei <jawoo@qq.com>
2023-12-24 20:40:39 +0800

    Update init.js

--- init.js
+++ init.js
@@ -1,6 +1,3 @@
-if (location.pathname.endsWith('/') === false) {
-  location.href = location.pathname.concat('/');
-}
 if (location.hostname !== 'localhost') {
   navigator.serviceWorker.register('/sw.js');
 }
